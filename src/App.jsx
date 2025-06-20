aa857f947c7ce8ccf1977afd342975425fc1ab25
Yosef Mihiret <yosefmihiret21@gmail.com>
2025-06-20 14:44:55 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Education from "./Components/Education/Education";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import BlurBlob from "./BlurBlob";
-import StartCanvas from "./Components/canvas/Stars";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
@@ -23,9 +22,6 @@ const App = () => {
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
         <div className="relative pt-20">
           <Navbar />
-          <body>
-            <StartCanvas />
-            <div>
               <About />
               <Skills />
               <Experience />
@@ -33,8 +29,6 @@ const App = () => {
               <Education />
               <Contact />
               <Footer />
-            </div>
-          </body>
           <Analytics />
           <SpeedInsights />
         </div>
