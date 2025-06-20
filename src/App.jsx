ddd1aefa82fd2d412d3abf3c71e5e8642fbaa8a3
Yosef Mihiret <yosefmihiret21@gmail.com>
2025-06-20 13:43:06 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ import Education from "./Components/Education/Education";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import BlurBlob from "./BlurBlob";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import StartCanvas from "./Components/canvas/Stars";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
   return (
@@ -22,13 +23,18 @@ const App = () => {
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
         <div className="relative pt-20">
           <Navbar />
-          <About />
-          <Skills />
-          <Experience />
-          <Work />
-          <Education />
-          <Contact />
-          <Footer />
+          <body>
+            <StartCanvas />
+            <div>
+              <About />
+              <Skills />
+              <Experience />
+              <Work />
+              <Education />
+              <Contact />
+              <Footer />
+            </div>
+          </body>
           <Analytics />
           <SpeedInsights />
         </div>
